fe7fd0cab73dd6fb6de4aac1ba9db6c1824e1e07
Matt2d3 <matttoodeetoo@gmail.com>
2022-11-27 12:46:37 -0500

    dissabled dev mode

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ engine.runAndWait()
 for index, name in enumerate(sr.Microphone.list_microphone_names()):
     print("Microphone with name \"{1}\" found for `Microphone(device_index={0})`".format(index, name))
 r = sr.Recognizer()
-devMode = True
+devMode = False
 running = True
 if(devMode == False):
     m = sr.Microphone() 
