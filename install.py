60a9f004bf6a93bd733373f19e8e7fc8c29b542d
Matt2d3 <matttoodeetoo@gmail.com>
2022-12-05 09:28:52 -0500

    edited installer and added a few files

--- install.py
+++ install.py
@@ -10,6 +10,7 @@ print("installing apt packages")
 os.system("apt install alsa-utils")
 os.system("apt install libespeak1")
 os.system("apt install net-tools")
+os.system("apt install wpasupplicant")
 os.system("apt install python3-rpi.gpio")
 os.system("apt install bpytop")
 print("installing pip3 packages")
@@ -18,21 +19,37 @@ os.system("pip3 install datetime")
 os.system("pip3 install speechRecognition")
 os.system("pip3 install pyttsx3")
 os.system("pip3 install gpiozero")
+os.system("pip3 install pyaudio")
 print("seting up start.service")
 os.system("mv start.service /etc/systemd/system/start.service")
 os.system("systemctl enable start")
 print("wifi setup")
-os.system("ifconfig wlan0 up")
-print("scanning for networks")
-os.system("ifconfig wlan0 up")
-print(os.system("nmcli dev wifi"))
-netName = input("enter the name of the network you wish to connect to as it appears above: ")
-password = input("enter network password (leave blank if none): ")
-if(password == ""):
-    os.system("iwconfig wlan0 essid "+netName)
-if(password != ""):
-    os.system("iwconfig wlan0 essid "+netName+" key "+password)
-print("i haven't implemented network tests, so.....")
-time.sleep(1)
+
+connectNet = False
+while(connectNet):
+
+    os.system("iw dev")
+    interface = input("please enter the name of the wifi interface as it appears above: ")
+    print("scanning for networks")
+    os.system("ifconfig "+interface+" up")
+    print(os.system("nmcli dev wifi"))
+    netName = input("enter the name of the network you wish to connect to as it appears above (leave blank if you wish to continue without wifi): ")
+    password = input("enter network password (leave blank if none): ")
+    if(netName == ""):
+        connectNet = False
+    
+    os.system("iwconfig "+interface+" essid "+netName)
+    if(password != ""):
+        os.system("iwconfig "+interface+" key "+password)
+    response = os.system("ping -c 1 -W 0.5 connectivity-check.ubuntu.com")
+    if(response == 0):
+        connectNet = False
+        print("connection confirmed")
+    else:
+        print("could not verify connection")
+print("")
+print("wifi connectivity is not currently supported")
+print("")
+time.sleep(5)
 print("restarting")
-os.system("shutdown -r now")
+#os.system("shutdown -r now")
---
+++ start.sh
@@ -0,0 +1,9 @@
+#! /bin/sh
+echo start.sh: starting main.py
+echo "      _____              _____"
+echo "|   | |     |     |      |   |"
+echo "|___| |____ |     |      |   |"
+echo "|   | |     |     |      |   |"
+echo "|   | |____ |____ |____  |___|"
+python3 main.py 
+
